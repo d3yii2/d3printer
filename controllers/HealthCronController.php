8346174d71a91e50c8420c1c2882057c2a6c7744
lauris@weberp.lv <latpages@gmail.com>
2020-11-02 10:56:59 +0200

    HealthCronController.php - extended from D3CommandController

--- controllers/HealthCronController.php
+++ controllers/HealthCronController.php
@@ -2,29 +2,32 @@
 
 namespace d3yii2\d3printer\controllers;
 
+use d3system\commands\D3CommandController;
 use d3yii2\d3printer\logic\D3PrinterHealth;
 use GuzzleHttp\Exception\GuzzleException;
-use yii\base\Exception;
 use Yii;
-use yii\console\Controller;
+use yii\base\Exception;
+use yii\console\ExitCode;
 
 /**
-* Class PrinterHealthController
-*/
-class HealthCronController extends Controller
+ * Class HealthCronController
+ * @package d3yii2\d3printer\controllers
+ */
+class HealthCronController extends D3CommandController
 {
     
     /**
      * @throws GuzzleException
      *  Cron example: /usr/bin/php <sitepath>/yii d3printer/health-cron
+     *  Console command: php yii d3printer-health-cron
      */
-    public function actionIndex()
+    public function actionIndex(): int
     {
         try {
             $health = new D3PrinterHealth();
             $message = $health->check();
             echo $message;
-        
+            return ExitCode::OK;
         } catch (Exception $e) {
             echo $e->getMessage();
             Yii::error($e->getMessage(), 'd3printer-error');
