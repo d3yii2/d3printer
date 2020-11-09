17b55d4a843efec4961277a18d423510f6a1d095
lauris@weberp.lv <latpages@gmail.com>
2020-11-09 10:23:47 +0200

    Default controller changed to console

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -2,35 +2,13 @@
 
 namespace d3yii2\d3printer\controllers;
 
-use d3yii2\d3printer\accessRights\D3PrinterFullUserRole;
 use d3yii2\d3printer\logic\read\D3PrinterReadConfiguration;
 use d3yii2\d3printer\logic\read\D3PrinterReadDevice;
 use yii\base\Exception;
-use yii\filters\AccessControl;
-use yii\web\Controller;
+use yii\console\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors(): array
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::class,
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'actions' => [
-                            'index',
-                        ],
-                        'roles' => [D3PrinterFullUserRole::NAME],
-                    ],
-                ],
-            ],
-        ];
-    }
     
     /**
      * Testing returned data
@@ -41,14 +19,24 @@ class DefaultController extends Controller
     {
         $printerDevice = new D3PrinterReadDevice();
         $status = $printerDevice->getStatus();
+        echo 'Status:' . PHP_EOL . $status . PHP_EOL;
+        
         $cartridgeRemaining = $printerDevice->getCartridgeRemaining();
+        echo 'Cartridge:' . PHP_EOL . $cartridgeRemaining . PHP_EOL;
+        
         $drumRemaining = $printerDevice->getDrumRemaining();
+        echo 'Drum:' . PHP_EOL . $drumRemaining . PHP_EOL;
         
         $printerConfig = new D3PrinterReadConfiguration();
         $paperSettings = $printerConfig->getPaperSettings();
+        echo 'Paper Settings:' . PHP_EOL . print_r($paperSettings, true);
+    
         $printSettings = $printerConfig->getPrintSettings();
+        echo 'Print Settings:' . PHP_EOL . print_r($printSettings, true);
+
         $energySettings = $printerConfig->getEnergySettings();
-        
+        echo 'Energy Settings:' . PHP_EOL . print_r($energySettings, true);
+    
         return true;
     }
 }
